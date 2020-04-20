13b87f59375007240bab093ef02d81daa4f1d6ae
lilawalker <lilawlker@gmail.com>
2020-04-20 22:13:44 +0100

    Tidy up test descriptions

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,6 +4,12 @@ describe Account do
 
   subject(:account) { described_class.new }
 
+  describe '.balance' do
+    it 'has an initial balance of 0' do
+      expect(account.balance).to eq 0
+    end
+  end
+
   describe '#deposit' do
     it 'responds to the method' do
       expect(account).to respond_to(:deposit).with(1).argument
@@ -11,12 +17,6 @@ describe Account do
 
     it 'adds the deposited amount to the balance' do
       expect { account.deposit(1000.00) }.to change { account.balance }.by(1000.00)
-    end
-  end
-
-  describe '.balance' do
-    it 'has an initial balance of 0' do
-      expect(account.balance).to eq 0
     end
   end
 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -2,23 +2,23 @@ describe 'User Stories' do
 
   let(:account) { Account.new }
 
-  describe 'deposit' do
-    # As a user
-    # So that I can store my money in a secure place
-    # I would like to deposit money into my account
-
-    it 'so money can be deposited into the account' do
-      expect { account.deposit(1000.00) }.not_to raise_error
-    end
-  end
-
   describe 'balance' do
     # As a user
     # So that I know how much money is in my account
     # I would like to see the balance on my account
 
-    it 'allows the balance to be displayed' do
+    it 'displays the balance' do
       expect { account.balance }.not_to raise_error
+    end
+  end
+
+  describe 'deposit' do
+    # As a user
+    # So that I can store my money in a secure place
+    # I would like to deposit money into my account
+
+    it 'can deposit money into the account' do
+      expect { account.deposit(1000.00) }.not_to raise_error
     end
   end
 
