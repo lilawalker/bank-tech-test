7febad08e7b85ac9bf7c59007588fe2f205fbda1
lilawalker <lilawlker@gmail.com>
2020-04-23 19:13:13 +0100

    Refactor methods to reduce line length

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,15 +2,7 @@ class Statement
 
   def format(transactions)
     transactions.reverse.map do |transaction|
-      date = date(transaction.time)
-      amount = decimal(transaction.amount)
-      balance = decimal(transaction.balance)
-
-      if transaction.type == 'credit'
-        "#{date} || #{amount} || || #{balance}"
-      else
-        "#{date} || || #{amount} || #{balance}"
-      end
+      row(transaction)
     end
   end
 
@@ -19,6 +11,24 @@ class Statement
   end
 
   private
+
+  def row(transaction)
+    line = formatted_transaction(transaction)
+
+    if transaction.type == 'credit'
+      "#{line[:date]} || #{line[:amount]} || || #{line[:balance]}"
+    else
+      "#{line[:date]} || || #{line[:amount]} || #{line[:balance]}"
+    end
+  end
+
+  def formatted_transaction(transaction)
+    {
+      date: date(transaction.time),
+      amount: decimal(transaction.amount),
+      balance: decimal(transaction.balance),
+    }
+  end
 
   def decimal(value)
     '%.2f' % value
